b3a5ba3d474d4711047cfeafe000242deb2031b7
bit1394 <it_nv@mail.ru>
2016-01-19 17:56:30 +0300

    #7_2 button in pixels on many scales

---
+++ P0072_LayoutScale/app/src/main/java/ru/bitreslab/p0072_layoutscale/Main.java
@@ -0,0 +1,13 @@
+package ru.bitreslab.p0072_layoutscale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
