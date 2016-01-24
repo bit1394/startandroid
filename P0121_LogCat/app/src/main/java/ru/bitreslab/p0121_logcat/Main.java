42e8be13b6ae6707d6a6c416a917727a1fb51b44
bit1394 <it_nv@mail.ru>
2016-01-24 21:46:04 +0300

    #12

---
+++ P0121_LogCat/app/src/main/java/ru/bitreslab/p0121_logcat/Main.java
@@ -0,0 +1,51 @@
+package ru.bitreslab.p0121_logcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity implements OnClickListener{
+
+    TextView textView;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        //1
+        button1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Нажата кнопка 1");
+            }
+        });
+        //
+
+        //2
+        button2.setOnClickListener(this);
+        //
+
+    }
+    //3
+    public void clickButton3(View view) {
+        textView.setText("Нажата кнопка 3");
+    }
+    //
+    @Override
+    public void onClick(View v) {
+        textView.setText("Нажата кнопка 2");
+    }
+}
