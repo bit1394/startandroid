c35a0bd36b49361e53a128e923ed7f2c719bdd7a
bit1394 <it_nv@mail.ru>
2016-01-24 21:57:20 +0300

    #12

--- P0121_LogCat/app/src/main/java/ru/bitreslab/p0121_logcat/Main.java
+++ P0121_LogCat/app/src/main/java/ru/bitreslab/p0121_logcat/Main.java
@@ -3,12 +3,15 @@ package ru.bitreslab.p0121_logcat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends AppCompatActivity implements OnClickListener{
+
+    private  static final String TAG = "myLogs";
 
     TextView textView;
     Button button1;
@@ -20,6 +23,8 @@ public class Main extends AppCompatActivity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
+        Log.d(TAG, "Найдем View-элементы");
         textView = (TextView) findViewById(R.id.textView);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
@@ -29,7 +34,12 @@ public class Main extends AppCompatActivity implements OnClickListener{
         button1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Нажата кнопка 1");
+                try {
+                    int i = 6/0;
+                    textView.setText("Нажата кнопка 1");
+                } catch (Exception e) {
+                    Log.d(TAG, "На ноль делить низзя!!", e);
+                }
             }
         });
         //
@@ -37,15 +47,16 @@ public class Main extends AppCompatActivity implements OnClickListener{
         //2
         button2.setOnClickListener(this);
         //
-
     }
     //3
     public void clickButton3(View view) {
+        Log.d(TAG, "Нажата кнопка 3");
         textView.setText("Нажата кнопка 3");
     }
     //
     @Override
     public void onClick(View v) {
+        Log.d(TAG, "Нажата кнопка 2");
         textView.setText("Нажата кнопка 2");
     }
 }
